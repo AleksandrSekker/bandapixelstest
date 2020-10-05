0284c2f2c395ebdce85b6c4bbf0bfe0845d246b2
AleksandrSekker <sekkerpleksandr@gmail.com>
2020-10-05 20:11:11 +0300

    Updates

---
+++ precache-manifest.f0eb47879d0a8cc63da4bfecc4e5a4cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5b1c045f22406b68d04d5d180b46e6",
+    "url": "/bandapixelstest/index.html"
+  },
+  {
+    "revision": "f5cdaf2eea9dbd666460",
+    "url": "/bandapixelstest/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "64a8db8bb54e902a7807",
+    "url": "/bandapixelstest/static/js/2.0e2fb339.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bandapixelstest/static/js/2.0e2fb339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5cdaf2eea9dbd666460",
+    "url": "/bandapixelstest/static/js/main.16ff7d1d.chunk.js"
+  },
+  {
+    "revision": "1c8698d87cc9a093803a",
+    "url": "/bandapixelstest/static/js/runtime-main.f967e32e.js"
+  }
+]);
